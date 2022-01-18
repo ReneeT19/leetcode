60acbc3a70b2365bd898cd59a1b4d83e6b6cd109
Renee Thomsen <rcthomsen19@gmail.com>
2022-01-17 22:31:27 -0500

    refactored

--- Easy/src/RomanToInteger.java
+++ Easy/src/RomanToInteger.java
@@ -3,24 +3,41 @@ public class RomanToInteger {
         convertRomanToInteger ob = new convertRomanToInteger();
         System.out.println(ob.romanToInt("III"));
         System.out.println(ob.romanToInt("IV"));
+        System.out.println(ob.romanToInt("MCMXCIV"));
+        for(int i=10; i>=0; --i){
+            System.out.print(i);
+        }
+        for(int i=10; i>=0; i--){
+            System.out.print(i);
+        }
     }
 
     //Java allows a class to be defined within another class. These are called Nested Classes using static keyword
     static class convertRomanToInteger {
-        public int romanToInt(String s) {
+        public int romanToInt(String roman) {
             int[] map = new int[256];
-            map['I'] = 1; map['V'] = 5; map['X'] = 10; map['L'] = 50; map['C'] = 100; map['D'] = 500; map['M'] = 1000;
+            int result = 0;
+            int previous = 1;
+            int current = 0;
+            map['I'] = 1;
+            map['V'] = 5;
+            map['X'] = 10;
+            map['L'] = 50;
+            map['C'] = 100;
+            map['D'] = 500;
+            map['M'] = 1000;
 
-            int ret = 0, pre = 1;
-            for (int i = s.length()-1; i >= 0; --i) {
-                int cur = map[s.charAt(i)];
-                if (cur < pre) ret -= cur;
-                else {
-                    pre = cur;
-                    ret += cur;
+            for (int i = roman.length() - 1; i >= 0; i--) {
+                current = map[roman.charAt(i)];
+                //it matters to check < condition first
+                if (current < previous) {
+                    result -= current; //result -= current
+                } else {
+                    previous = current;
+                    result += current;
                 }
             }
-            return ret;
+            return result;
         }
     }
 }
