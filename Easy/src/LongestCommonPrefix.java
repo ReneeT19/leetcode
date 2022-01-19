3839f6f412714749e50bdbf9edf236659885d03b
Renee Thomsen <rcthomsen19@gmail.com>
2022-01-18 22:40:19 -0500

    added longest common prefix practice method

--- Easy/src/LongestCommonPrefix.java
+++ Easy/src/LongestCommonPrefix.java
@@ -11,10 +11,31 @@ Explanation: There is no common prefix among the input strings.
 */
     public static void main(String[] args) {
         GetLongestCommonPrefix getLongestCommonPrefix = new GetLongestCommonPrefix ();
-
+        String[] strs =  new String[] { "flower", "flow", "flight"};
+        String[] strs2 =  new String[] {"dog","racecar","car"};
+        String[] strs3 =  new String[] { "chin", "china", "chinese", "chinaberry"};
+        System.out.println(getLongestCommonPrefix.returnLongestCommonPrefix(strs));
+        System.out.println(getLongestCommonPrefix.returnLongestCommonPrefix(strs2));
+        System.out.println(getLongestCommonPrefix.returnLongestCommonPrefix(strs3));
     }
 
     static class GetLongestCommonPrefix {
-
+        public static String returnLongestCommonPrefix(String[] strs) {
+            int size = strs.length;
+            String commonPrefix = "";
+            int i = 0;
+            while(i < size - 1) {
+//                if(strs[i].length() < strs[i+1].length()) {
+//                    int length = strs[i].length();
+//                }
+                if(strs[i].charAt(i) == strs[i+1].charAt(i)) {
+                    commonPrefix += Character.toString(strs[i].charAt(i));
+                    i++;
+                } else {
+                    return "";
+                }
+            }
+            return commonPrefix;
+        }
     }
 }
