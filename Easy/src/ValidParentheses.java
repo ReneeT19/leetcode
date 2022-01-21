0ae2ca070edcb3257f0a1cdcfd6118fb96262e75
Renee Thomsen <rcthomsen19@gmail.com>
2022-01-20 22:35:01 -0500

    added valid parentheses practice

---
+++ Easy/src/ValidParentheses.java
@@ -0,0 +1,9 @@
+public class ValidParentheses {
+    public static void main(String[] args) {
+        isValid("()[]{}");
+    }
+
+    public static boolean isValid(String s) {
+        return true;
+    }
+}
