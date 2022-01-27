8a9d93a3126b840d4bc50dee0d7eb60955a19dda
Renee Thomsen <rcthomsen19@gmail.com>
2022-01-27 15:25:22 -0500

    developed on parentheses practice

--- Easy/src/ValidParentheses.java
+++ Easy/src/ValidParentheses.java
@@ -1,9 +1,24 @@
 public class ValidParentheses {
     public static void main(String[] args) {
-        isValid("()[]{}");
+        System.out.println(isValid("()[]{}"));
+        isValid("(){}");
+        isValid("()");
+        isValid("()(]{}");
+        isValid("(]");
     }
 
     public static boolean isValid(String s) {
+        int i = 0;
+        while (i < s.length()) {
+            char leftPa = s.charAt(i);
+            char rightPa = s.charAt(i + 1);
+            i += 2;
+            if (leftPa != rightPa) {
+                return false;
+            } else {
+                return true;
+            }
+        }
         return true;
     }
 }
