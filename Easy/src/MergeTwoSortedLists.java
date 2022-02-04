96e7b3cb3a05f1fc8738febd0d77fd362aac5e96
Renee Thomsen <rcthomsen19@gmail.com>
2022-02-04 15:21:18 -0500

    added notes

--- Easy/src/MergeTwoSortedLists.java
+++ Easy/src/MergeTwoSortedLists.java
@@ -2,7 +2,7 @@ public class MergeTwoSortedLists {
     public static void main(String[] args) {
 
     }
-    
+
     // Definition for singly-linked list.
     public class ListNode {
         int val;
